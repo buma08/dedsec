94e7ac70ff3a8ada26f25d5d4709a19ac281c50b
buma08 <dbuges10@gmail.com>
2023-02-06 14:53:02 +0100

    navBar mobile first

--- blogger/src/components/NavBar.jsx
+++ blogger/src/components/NavBar.jsx
@@ -1,39 +1,55 @@
-import '../css/Nav.css'
-import UseQuery from '../hooks/useQuery'
-import {TbMenu} from 'react-icons/tb'
-function NavBar(){
+import "../css/Nav.css";
+import UseQuery from "../hooks/useQuery";
+import { TbMenu } from "react-icons/tb";
+import {RxCross2} from 'react-icons/rx'
+function NavBar() {
+  const url = window.location.href;
+  let page = url.slice(22);
+  console.log(page);
+  if (page === "") {
+    page = "home";
+  }
+  function isActive(p) {
+    if (p === page) {
+      return " active";
+    } else {
+      return "";
+    }
+  }
+  function ToggleMenu() {
+    document.getElementById("navBar-id").classList.toggle("navBar__active");
+  }
+  return (
 
-    const url = window.location.href
-    let page = url.slice(22)
-    console.log(page)
-    if(page === ''){
-        page = 'home'
-    }
-    function isActive(p){
-
-        if(p === page){
-            return ' active'
-        }
-        else{
-            return ''
-        }
-    }
-    function ToggleMenu(){
-        document.getElementById('navBar-id').classList.toggle('navBar__active')
-    }
-    return(
-        <>
-        <nav className='navBar__active' id='navBar-id'>
-            <p className={"navBar__item" + isActive('home')}>Home</p>
-            <p className={"navBar__item" + isActive('notes')}>Notes</p>
-            <p className={"navBar__item" + isActive('writeup')}>Write-up</p>
-            <p className={"navBar__item" + isActive('feedback')}>FeedBack</p>
-            <p className={"navBar__item" + isActive('admin')}>Admin</p>
-        </nav>
-        <TbMenu className='navBar_button' onClick={ToggleMenu}></TbMenu>
-        </>
+    <>
+    <div className="navBar__button-open-div">
+        <TbMenu onClick={ToggleMenu} id=' navbar__button-open'></TbMenu>
+    </div>
     
+      <nav className="navBar" id="navBar-id">
+        <RxCross2 className="navBar_button" onClick={ToggleMenu}></RxCross2>
+        <li className="navBar__li">
+          <p className={"navBar__item" + isActive("home")}>Home</p>
+        </li>
+       
+        <li className="navBar__li">
+          <p className={"navBar__item" + isActive("notes")}>Notes</p>
+        </li>
         
-    )
+        <li className="navBar__li">
+          <p className={"navBar__item" + isActive("writeup")}>Write-up</p>
+        </li>
+        
+        <li className="navBar__li">
+          <p className={"navBar__item" + isActive("feedback")}>FeedBack</p>
+        </li>
+        
+        <li className="navBar__li">
+          <p className={"navBar__item" + isActive("admin")}>Admin</p>
+        </li>
+        
+      </nav>
+    </>
+  );
 }
-export default NavBar
+export default NavBar;
--- blogger/src/pages/Home.jsx
+++ blogger/src/pages/Home.jsx
@@ -3,9 +3,10 @@ import NavBar from "../components/NavBar"
 function HomePage(){
     return(
         <>
+   
         <NavBar></NavBar>
 
-        <h1>Home</h1>
+        
         </>
     )
 }
